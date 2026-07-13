af288eb980c028a0e735ee8fb21c0af588d561df
rlauff <r.lauff1997@gmail.com>
2026-07-13 21:51:58 +0200

    fixed submodule quiver mutations

--- build_wasms.sh
+++ build_wasms.sh
@@ -13,11 +13,14 @@ rustup target list --installed 2>/dev/null | grep -q wasm32-unknown-unknown || {
 
 cd rhombic_strips
 echo "Building rhombic_strips wasm…"
-
 wasm-pack build --target web --out-dir www/pkg --release
 
 cd ..
 cd subtext
 echo "Building subtext wasm…"
+wasm-pack build --target web
 
+cd ..
+cd quiver_mutations
+echo "Building quiver_mutations wasm…"
 wasm-pack build --target web
